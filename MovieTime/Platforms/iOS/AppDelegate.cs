6044a80eb627aa3f16c4df6cbfcc00fa86401ad7
xMatysz <matizek56@gmail.com>
2022-11-08 18:05:46 +0100

    Add project files.

---
+++ MovieTime/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MovieTime;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
